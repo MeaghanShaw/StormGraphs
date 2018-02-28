04e3615d7747382334cf502a243a7bf0093d94d0
Meaghan Shaw <meaghanshaw@Meaghans-MacBook-Air.local>
2018-02-28 11:45:58 -0500

    Manipulating time

--- StormGraphs.R
+++ StormGraphs.R
@@ -5,3 +5,11 @@ stormevents<-read.csv(file="StormEvents.csv",header=TRUE)
 colnames(stormevents)<-newname
 
 # Pull out date and time
+
+library(lubridate)
+stormevents$newdate<-mdy_hm(stormevents$date_and_time)
+stormevents$year<-year(stormevents$newdate)
+stormevents$doy<-yday(stormevents$newdate)
+stormevents$hour<-hour(stormevents$newdate)
+stormevents$min<-minute(stormevents$newdate)
+stormevents$time<-stormevents$hour + stormevents$min/60
