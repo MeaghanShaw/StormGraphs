5f897ddace2e59b92da903ca3961a857fd6242b0
Meaghan Shaw <meaghanshaw@Meaghans-MacBook-Air.local>
2018-02-28 11:55:05 -0500

    Mg/L to mmol/L and scatterplot matrix

--- StormGraphs.R
+++ StormGraphs.R
@@ -13,3 +13,17 @@ stormevents$doy<-yday(stormevents$newdate)
 stormevents$hour<-hour(stormevents$newdate)
 stormevents$min<-minute(stormevents$newdate)
 stormevents$time<-stormevents$hour + stormevents$min/60
+
+# Transform mg/L to mmol/L
+
+stormevents$Al_mmolL<-(stormevents$Al_mgL/26.982)
+stormevents$Ca_mmolL<-(stormevents$Ca_mgL/40.08)
+stormevents$Fe_mmolL<-(stormevents$Fe_mgL/55.845)
+stormevents$K_mmolL<-(stormevents$K_mgL/39.099)
+stormevents$Mg_mmolL<-(stormevents$Mg_mgL/24.305)
+stormevents$Mn_mmolL<-(stormevents$Mn_mgL/54.938)
+stormevents$Na_mmolL<-(stormevents$Na_mgL/22.99)
+stormevents$SO42_mmolL<-(stormevents$SO42_mgL/96.06)
+
+# Scatter plot matrix for Al to SO42
+pairs(stormevents[20:27])
