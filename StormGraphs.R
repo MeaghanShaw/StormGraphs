3b55d99a0d9ee5cdf88bd0316b83789d71471d48
Meaghan Shaw <meaghanshaw@Meaghans-MacBook-Air.local>
2018-03-07 13:32:28 -0500

    Made all Fe/Mn storm event graphs

--- StormGraphs.R
+++ StormGraphs.R
@@ -33,7 +33,7 @@ Jul717storm<-stormevents[which(stormevents$doy == "188"),]
 library(ggplot2)
 yscale<-10
 
-two_ys<-ggplot(Jul717storm,aes(x=newdate))+
+Jul_7_storm<-ggplot(Jul717storm,aes(x=newdate))+
   geom_line(aes(y=Q_m3s),colour="blue",size=1.2)+
   geom_point(aes(y=Fe_mmolL/yscale,fill="orange"),colour="black",pch=21,size=2)+
   geom_point(aes(y=Mn_mmolL/yscale,fill="green"),pch=21,colour="black",size=2)+
@@ -44,7 +44,93 @@ two_ys<-ggplot(Jul717storm,aes(x=newdate))+
        x="Date")+
   theme_bw()
 
-two_ys
+Jul_7_storm
 
 #Pull out processed data sheet
 write.csv(Jul717storm,"July717storm.csv")
+
+# Plot July 13 event for Fe and Mn
+Jul1317storm<-stormevents[which(stormevents$doy == "194"),]
+library(ggplot2)
+yscale<-10
+
+Jul_13_storm<-ggplot(Jul1317storm,aes(x=newdate))+
+  geom_line(aes(y=Q_m3s),colour="blue",size=1.2)+
+  geom_point(aes(y=Fe_mmolL/yscale,fill="orange"),colour="black",pch=21,size=2)+
+  geom_point(aes(y=Mn_mmolL/yscale,fill="green"),pch=21,colour="black",size=2)+
+  scale_y_continuous(sec.axis=sec_axis(~.*yscale,name="Concentration"))+
+  scale_fill_manual(guide="legend",name="ions",
+                    values=c("orange","green"),labels=c("Fe","Mn"))+
+  labs(y="Discharge",
+       x="Date")+
+  theme_bw()
+
+Jul_13_storm
+
+#Pull out processed data sheet
+write.csv(Jul1317storm,"July1317storm.csv")
+
+# Plot October 8-9 event for Fe and Mn
+Oct8917storm<-stormevents[which(stormevents$doy == "281"
+                         | stormevents$doy == "282"),]
+library(ggplot2)
+yscale<-10
+
+Oct_8_9_storm<-ggplot(Oct8917storm,aes(x=newdate))+
+  geom_line(aes(y=Q_m3s),colour="blue",size=1.2)+
+  geom_point(aes(y=Fe_mmolL/yscale,fill="orange"),colour="black",pch=21,size=2)+
+  geom_point(aes(y=Mn_mmolL/yscale,fill="green"),pch=21,colour="black",size=2)+
+  scale_y_continuous(sec.axis=sec_axis(~.*yscale,name="Concentration"))+
+  scale_fill_manual(guide="legend",name="ions",
+                    values=c("orange","green"),labels=c("Fe","Mn"))+
+  labs(y="Discharge",
+       x="Date")+
+  theme_bw()
+
+Oct_8_9_storm
+
+#Pull out processed data sheet
+write.csv(Oct8917storm,"October8917storm.csv")
+
+# Plot October 11 event for Fe and Mn
+Oct1117storm<-stormevents[which(stormevents$doy == "284"),]
+library(ggplot2)
+yscale<-10
+
+Oct_11_storm<-ggplot(Oct1117storm,aes(x=newdate))+
+  geom_line(aes(y=Q_m3s),colour="blue",size=1.2)+
+  geom_point(aes(y=Fe_mmolL/yscale,fill="orange"),colour="black",pch=21,size=2)+
+  geom_point(aes(y=Mn_mmolL/yscale,fill="green"),pch=21,colour="black",size=2)+
+  scale_y_continuous(sec.axis=sec_axis(~.*yscale,name="Concentration"))+
+  scale_fill_manual(guide="legend",name="ions",
+                    values=c("orange","green"),labels=c("Fe","Mn"))+
+  labs(y="Discharge",
+       x="Date")+
+  theme_bw()
+
+Oct_11_storm
+
+#Pull out processed data sheet
+write.csv(Oct1117storm,"October1117storm.csv")
+
+# Plot November 15-16 event for Fe and Mn
+Nov151617storm<-stormevents[which(stormevents$doy == "319"
+                                | stormevents$doy == "320"),]
+library(ggplot2)
+yscale<-10
+
+Nov_15_16_storm<-ggplot(Nov151617storm,aes(x=newdate))+
+  geom_line(aes(y=Q_m3s),colour="blue",size=1.2)+
+  geom_point(aes(y=Fe_mmolL/yscale,fill="orange"),colour="black",pch=21,size=2)+
+  geom_point(aes(y=Mn_mmolL/yscale,fill="green"),pch=21,colour="black",size=2)+
+  scale_y_continuous(sec.axis=sec_axis(~.*yscale,name="Concentration"))+
+  scale_fill_manual(guide="legend",name="ions",
+                    values=c("orange","green"),labels=c("Fe","Mn"))+
+  labs(y="Discharge",
+       x="Date")+
+  theme_bw()
+
+Nov_15_16_storm
+
+#Pull out processed data sheet
+write.csv(Nov151617storm,"November151617storm.csv")
